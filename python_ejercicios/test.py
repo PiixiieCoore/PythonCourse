7bc95f57576deef15d18204013a5acec72c17ff7
PiixiieCoore <bmth_coore@hotmail.com>
2024-02-06 00:06:54 -0600

    #

--- python_ejercicios/test.py
+++ python_ejercicios/test.py
@@ -1,15 +1,1 @@
-def maybe_double_last(hand):
-    """Multiply a Jack card value in the last index position by 2.
-
-    :param hand: list - cards in hand.
-    :return: list - hand with Jacks (if present) value doubled.
-    """
-
-    if hand[-1] == 11:
-        hand[-1] = 22
-    return hand
-
-hand = [5, 9, 11]
-# hand = [5, 9, 10]
-
-print(maybe_double_last(hand))
+print('hola')
